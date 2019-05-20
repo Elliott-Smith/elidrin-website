cf0305bdced99ee4c817f4e6056f1f349de4a9d2
Elliott <8598148+Elliott-Smith@users.noreply.github.com>
2019-05-20 20:56:49 +1000

    Updated privacy policy

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,42 +9,42 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Privacy Policy</h1>
-    <p>
-    1. We respect your privacy <br>
+    <pre>
+    1. We respect your privacy
     1.1. Elidrin respects your right to privacy and is committed to safeguarding the privacy
     of our customers and software application users. We adhere to the National Privacy
     Principles established by the Privacy Act 1988 (Cth). This policy sets out how we collect
-    and treat your personal information.<br>
+    and treat your personal information.
     1.2. "Personal information" is information we hold which is identifiable as being about
-    you.<br>
-    2. Collection of personal information<br>
+    you.
+    2. Collection of personal information
     2.1. Elidrin will, from time to time, receive and store personal information you enter onto
-    our software application Galaxy Miner, provide to us directly or give to us in other forms.<br>
+    our software application Galaxy Miner, provide to us directly or give to us in other forms.
     2.2. You may provide basic information such as your name, phone number, address
     and email address to enable us to send information, provide updates and process your
     product or service order. We may collect additional information at other times, including
     but not limited to, when you provide feedback, when you provide information about your
     personal or business affairs, change your content or email preference, respond to
     surveys and/or promotions, provide financial or credit card information, or communicate
-    with our customer support.<br>
+    with our customer support.
     2.3. Additionally, we may also collect any other information you provide while interacting
-    with us.<br>
-    3. How we collect your personal information<br>
+    with us.
+    3. How we collect your personal information
     3.1. Elidrin collects personal information from you in a variety of ways, including when
     you interact with us electronically or in person, when you access our software application
     and when we provide our services to you. We may receive personal information from
-    third parties. If we do, we will protect it as set out in this Privacy Policy.<br>
-    4. Use of your personal information<br>
+    third parties. If we do, we will protect it as set out in this Privacy Policy.
+    4. Use of your personal information
     4.1. Elidrin may use personal information collected from you to provide you with
     information, updates and our services. We may also make you aware of new and
     additional products, services and opportunities available to you. We may use your
     personal information to improve our products and services and better understand your
-    needs.<br>
+    needs.
     4.2. The Application may make third party social media features available to its users.
     We cannot ensure the security of any information you choose to make public in a social
     media feature. Also, we cannot ensure that parties who have access to such publicly
-    available information will respect your privacy.<br>
-    </p>
+    available information will respect your privacy.
+    </pre>
   </Layout>
 )
 
