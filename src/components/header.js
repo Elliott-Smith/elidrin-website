66df2872f7b088349fb53ce665da18d984fc0985
Elliott <8598148+Elliott-Smith@users.noreply.github.com>
2019-05-20 20:47:46 +1000

    Reverted header changes

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: lightslategrey,
+      background: `rebeccapurple`,
       marginBottom: `1.45rem`,
     }}
   >
